d20fd99d1d995a726e9be7820a3cd1739514046a
patdalcia <patdalcia@gmail.com>
2025-08-28 00:46:36 -0700

    UPDATED 2022 day 1, made it WAY better

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,83 +1,24 @@
-fn part_one(input: &str) -> i32 {
-    let input_as_array = input.lines();
-    let mut total_per_elf = vec![];
-    let mut temp_totals = vec![];
-    for line in input_as_array {
-        if line.is_empty() {
-            let mut temp_total = 0;
-            temp_totals.iter().for_each(|temp| {
-                temp_total += temp;
-            });
-            total_per_elf.push(temp_total);
-            temp_totals.clear();
-        } else if let Ok(number) = line.parse::<i32>() {
-            temp_totals.push(number);
+use std::ops::AddAssign;
+
+fn solve_puzzle(input: &str) -> Vec<u32> {
+    let mut temp_total = 0;
+    let mut totals = vec![];
+    for line in input.lines() {
+        if let Ok(num) = line.parse::<u32>() {
+            temp_total.add_assign(num);
+        } else {
+            totals.push(temp_total);
+            temp_total = 0;
         }
     }
-    if !(temp_totals.is_empty()) {
-        let mut temp_total = 0;
-        temp_totals.iter().for_each(|temp| {
-            temp_total += temp;
-        });
-        total_per_elf.push(temp_total);
-        temp_totals.clear();
-    }
-    let mut highest_calorie_count = 0;
-    for total in total_per_elf {
-        if total > highest_calorie_count {
-            highest_calorie_count = total;
-        }
-    }
-    highest_calorie_count
-}
-fn part_two(input: &str) -> i32 {
-    let input_as_array = input.lines();
-    let mut total_per_elf = vec![];
-    let mut temp_totals = vec![];
-    for line in input_as_array {
-        if line.is_empty() {
-            let mut temp_total = 0;
-            temp_totals.iter().for_each(|temp| {
-                temp_total += temp;
-            });
-            total_per_elf.push(temp_total);
-            temp_totals.clear();
-            continue;
-        }
-        if let Ok(number) = line.parse::<i32>() {
-            temp_totals.push(number);
-        }
-    }
-    if !(temp_totals.is_empty()) {
-        let mut temp_total = 0;
-        temp_totals.iter().for_each(|temp| {
-            temp_total += temp;
-        });
-        total_per_elf.push(temp_total);
-        temp_totals.clear();
-    }
-    let mut highest_calorie_count = 0;
-    let mut second_highest_calorie_count = 0;
-    let mut third_highest_calorie_count = 0;
-    for total in total_per_elf {
-        if total > highest_calorie_count {
-            third_highest_calorie_count = second_highest_calorie_count;
-            second_highest_calorie_count = highest_calorie_count;
-            highest_calorie_count = total;
-        } else if total > second_highest_calorie_count {
-            third_highest_calorie_count = second_highest_calorie_count;
-            second_highest_calorie_count = total;
-        } else if total > third_highest_calorie_count {
-            third_highest_calorie_count = total;
-        }
-    }
-    highest_calorie_count + second_highest_calorie_count + third_highest_calorie_count
+    totals.push(temp_total);
+    totals.sort_by(|a, b| b.cmp(a));
+    totals
 }
 
 fn main() {
     let input = include_str!("../puzzle_input.txt");
-    let answer_to_part_one = part_one(input);
-    let answer_to_part_two = part_two(input);
-    println!("ANSWER TO PART ONE: {answer_to_part_one}");
-    println!("ANSWER TO PART TWO: {answer_to_part_two}");
+    let totals = solve_puzzle(input);
+    println!("ANSWER TO PART ONE: {}", totals[0]);
+    println!("ANSWER TO PART TWO: {}", totals[0] + totals[1] + totals[2]);
 }
