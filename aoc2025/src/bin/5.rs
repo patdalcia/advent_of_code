bf809672bf20534ab56fda910fe1f1056b6358a4
patdalcia <patdalcia@gmail.com>
2026-01-01 23:16:32 -0700

    wrote a cool little algorith to remove overlap in ranges

--- aoc2025/src/bin/5.rs
+++ aoc2025/src/bin/5.rs
@@ -1,16 +1,35 @@
 use aoc2025::get_puzzle;
-use indicatif::ProgressIterator;
-use std::{thread::sleep, time::Duration};
+use std::cmp;
 
-fn solve_puzzle(input: &str) -> (u64, usize) {
+// A nice little algorithm
+fn merge_overlapping_ranges(arr: &mut [(u64, u64)]) -> Vec<(u64, u64)> {
+    arr.sort_by(|a, b| a.0.cmp(&b.0));
+    let mut result: Vec<(u64, u64)> = Vec::new();
+    result.push(arr[0]);
+
+    for i in 1..arr.len() {
+        let current: (u64, u64) = arr[i];
+        let j: usize = result.len() - 1;
+
+        if current.0 >= result[j].0 && current.0 <= result[j].1 {
+            result[j].1 = cmp::max(current.1, result[j].1);
+        } else if current.1 >= result[j].0 && current.1 <= result[j].1 {
+            result[j].0 = cmp::min(current.0, result[j].0);
+        } else if !result.contains(&current) {
+            result.push(current);
+        }
+    }
+    result
+}
+
+fn solve_puzzle(input: &str) -> (u64, u64) {
     println!("~~INPUT~~\n{input}");
     let mut fresh_count = 0;
-    let mut available_fresh_ids: Vec<u64> = Vec::new();
     let (before, after) = input
         .trim()
         .split_once("\n\n")
         .expect("COULD NOT SPLIT INPUT AT BLANK LINE");
-    let ranges: Vec<(u64, u64)> = before
+    let mut ranges: Vec<(u64, u64)> = before
         .lines()
         .map(|line| line.split_once('-').expect("COULD NOT SPLIT LINE ON -"))
         .filter_map(|(start_str, end_str)| {
@@ -24,15 +43,15 @@ fn solve_puzzle(input: &str) -> (u64, usize) {
         })
         .collect();
 
+    let condensed_ranges = merge_overlapping_ranges(&mut ranges);
+
     let ingredient_ids: Vec<u64> = after
         .lines()
         .filter_map(|line| line.trim().parse::<u64>().ok())
         .collect();
-    println!("Parsed Ranges: {ranges:?}");
-    println!("Parsed Ingredient Id's: {ingredient_ids:?}");
 
     for id in ingredient_ids {
-        for (start, end) in &ranges {
+        for (start, end) in &condensed_ranges {
             let range = start..=end;
             if range.contains(&&id) {
                 fresh_count += 1;
@@ -41,14 +60,12 @@ fn solve_puzzle(input: &str) -> (u64, usize) {
         }
     }
 
-    for (start, end) in ranges.iter().progress() {
-        for id in *start..=*end {
-            if !available_fresh_ids.contains(&id) {
-                available_fresh_ids.push(id);
-            }
-        }
+    let mut answer_part_2 = 0;
+    for range in &condensed_ranges {
+        answer_part_2 += range.1.abs_diff(range.0) + 1;
     }
-    (fresh_count, available_fresh_ids.len())
+
+    (fresh_count, answer_part_2)
 }
 
 fn main() {
